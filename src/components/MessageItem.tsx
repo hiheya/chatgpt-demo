cbaffb6d569286ce635b638fba7cde057b45f00b
liaochongrong <chongrong.liao@urovo.com>
2023-04-10 20:11:10 +0800

    修改头像

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -18,7 +18,8 @@ export default ({ role, message, showRetry, onRetry }: Props) => {
   const roleClass = {
     system: 'bg-gradient-to-r from-gray-300 via-gray-200 to-gray-300',
     user: 'bg-gradient-to-r from-purple-400 to-yellow-400',
-    assistant: 'bg-gradient-to-r from-yellow-200 via-green-200 to-green-300',
+    // assistant: 'bg-gradient-to-r from-yellow-200 via-green-200 to-green-300',
+    assistant: 'url(icon.svg)'
   }
   const [source] = createSignal('')
   const { copy, copied } = useClipboard({ source, copiedDuring: 1000 })
@@ -71,11 +72,7 @@ export default ({ role, message, showRetry, onRetry }: Props) => {
   return (
     <div class="py-2 -mx-4 px-4 transition-colors md:hover:bg-slate/3">
       <div class="flex gap-3 rounded-lg" class:op-75={role === 'user'}>
-        {/* <div class={`shrink-0 w-7 h-7 mt-4 rounded-full op-80 ${roleClass[role]}`} /> */}
-        <div class="shrink-0 w-7 h-7 mt-4 rounded-full op-80">
-          <img src="icon.svg" alt="头像" class="rounded-full w-full h-full object-cover" />
-        </div>
-
+        <div class={`shrink-0 w-7 h-7 mt-4 rounded-full op-80 ${roleClass[role]}`} />
         <div class="message prose break-words overflow-hidden" innerHTML={htmlString()} />
       </div>
       {showRetry?.() && onRetry && (
