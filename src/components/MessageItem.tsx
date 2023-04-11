bc66777513d81418e68aa6c5d0b358463a16d151
liaochongrong <chongrong.liao@urovo.com>
2023-04-11 10:28:12 +0800

    头像不生效问题

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -83,7 +83,7 @@ export default ({ role, message, showRetry, onRetry }: Props) => {
     <div class="py-2 -mx-4 px-4 transition-colors md:hover:bg-slate/3">
       <div class="flex gap-3 rounded-lg" class:op-75={role === 'user'}>
         <div class={`shrink-0 w-7 h-7 mt-4 rounded-full op-80 ${roleClass[role]}`}>
-          {roleClass[role] === 'assistant' ? <AssistantIcon /> : null}
+          { role === 'assistant' ? <AssistantIcon /> : null }
         </div>
         <div class="message prose break-words overflow-hidden" innerHTML={htmlString()} />
       </div>
